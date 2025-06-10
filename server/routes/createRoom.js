e990762235eb93c0ec2bafe241586a723c4ffccb
Jatin <jatin22101@iiitnr.edu.in>
2025-06-11 01:33:20 +0530

    fix

--- server/routes/createRoom.js
+++ server/routes/createRoom.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { nanoid } = require('nanoid');
 const router = express.Router();
+require('dotenv').config();
+const UI_URL = process.env.UI_URL;
 
 router.get('/', (req, res) => {
   const roomId = nanoid(10); // e.g. 'JtQnVZxK9Q'
-  res.json({ roomId, link: `https://localhost:5173/meeting/${roomId}` });
+  res.json({ roomId, link: `${UI_URL}/meeting/${roomId}` });
 });
 
 module.exports = router;
